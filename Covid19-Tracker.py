935eefefdef34d9bf1bf33fa14f1b3a273f75296
Dawson Fields <dawson@gofields.com>
2020-03-15 10:47:55 -0500

    Add Logging of Days

--- Covid19-Tracker.py
+++ Covid19-Tracker.py
@@ -5,12 +5,12 @@ import pandas as pd
 import plotly.express as px
 import json
 
+from datetime import date
 
 def prepareData(url, geoJson):
 	print("Grabbing Data...")
 
 	page = requests.get(url, verify=False)
-
 	html = BeautifulSoup(page.content, 'html.parser')
 	table = html.find(summary="Texas COVID-19 Cases")
 
@@ -54,13 +54,16 @@ def drawFigure(geoJson, maxRange):
 						)
 	fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 	fig.update_geos(fitbounds="locations")
-	fig.show()
-
+	return fig
 
 def main():
 	texas = json.load(open("texas.json"))
 	data = prepareData(geoJson=texas, url="https://dshs.texas.gov/news/updates.shtm#coronavirus")
-	drawFigure(geoJson=texas, maxRange=max(data, key=lambda x: x[1])[1])
+	figure = drawFigure(geoJson=texas, maxRange=max(data, key=lambda x: x[1])[1])
+	figure.show()
 
+	output = "tracking/" + str(date.today().strftime("%d-%m-%Y")) +".html"
+	print("Writing to file: " + output)
+	figure.write_html(output)
 
 main()
